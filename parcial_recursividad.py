a9d6d1820f35a0abbae92575a65c3dc3d7e9ab85
Jalegosa <jalegosa01@gmail.com>
2025-07-30 19:17:42 -0600

    cont

---
+++ parcial_recursividad.py
@@ -0,0 +1,90 @@
+#corrige cada una de las funciones para que resulva cada ejercicio y agregarlo en un menu match case en el
+#que tenga opciones y que la última sea salir
+#la segunda indica cuantas veces cesea repetir una palabra, y primero pide la palabra
+#la tercera cuenta la cantidad de veces en la que la letra que aparece en la palabra
+# y la cuarta dejarla para una posible funcion de un numero decimal a binario
+# # la 5ta cuenta cuantos digitos tiene el numero que ha ingresado el usuario
+#la primera va la opcion del maximo comun divisor en base al algoritmo de euclides, que debe pedir dos numero a y b es igual al MCD de b y a % b y se repite hasta que el residuo sea 0
+
+def mcd_numeros(numero1,numero2)
+    if numero1 % 0  and numero2 % 2:
+        return
+    else:
+        mcd_num = numero1 %
+
+def repetir_palabra(n, palabra):
+    if n == 0:
+       return
+    else:
+        palabra =
+        print(palabra)
+
+palabra = input("Ingrese una palabra: ")
+n = int(input("¿Cuántas veces desea repetir"))
+
+repetir_palabra(n)
+
+def contar_letra(letra):
+    if n ==  1:
+    else:
+        print (f"La letra {letra}  aparece  {n} veces")
+
+palabra_contar = input("Ingrese la cadena: ")
+ingrese_letra = input("Ingrese la letra que desee contar de la cadena: ")
+print(contar_letra(letra))
+
+def contador_digitos():
+    if numero == 0:
+        return
+    else:
+        contar_numero = count.numero
+        print(f"el total de digitos es: {contar_numero}")
+
+numero = int(input("Ingrese un numero: "))
+contador_digitos()
+
+
+#corrige cada una de las funciones para que resulva cada ejercicio y agregarlo en un menu match case en el
+#que tenga opciones y que la última sea salir
+#la segunda indica cuantas veces cesea repetir una palabra, y primero pide la palabra
+#la tercera cuenta la cantidad de veces en la que la letra que aparece en la palabra
+# y la cuarta dejarla para una posible funcion de fibonachi
+# # la 5ta cuenta cuantos digitos tiene el numero que ha ingresado el usuario
+#la primera va la opcion del maximo comun divisor en base al algoritmo de euclides, que debe pedir dos numero a y b es igual al MCD de b y a % b y se repite hasta que el residuo sea 0
+
+def mcd_numeros(numero1,numero2)
+    if numero1 % 0  and numero2 % 2:
+        return
+    else:
+        mcd_num = numero1 %
+
+def repetir_palabra(n, palabra):
+    if n == 0:
+       return
+    else:
+        palabra =
+        print(palabra)
+
+palabra = input("Ingrese una palabra: ")
+n = int(input("¿Cuántas veces desea repetir"))
+
+repetir_palabra(n)
+
+def contar_letra(letra):
+    if n ==  1:
+    else:
+        print (f"La letra {letra}  aparece  {n} veces")
+
+palabra_contar = input("Ingrese la cadena: ")
+ingrese_letra = input("Ingrese la letra que desee contar de la cadena: ")
+print(contar_letra(letra))
+
+def contador_digitos():
+    if numero == 0:
+        return
+    else:
+        contar_numero = count.numero
+        print(f"el total de digitos es: {contar_numero}")
+
+numero = int(input("Ingrese un numero: "))
+contador_digitos()
