34ba290f2f5032cef668c4689063169b17499be7
Jalegosa <jalegosa01@gmail.com>
2025-07-30 18:51:47 -0600

    cont

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
 from itertools import count
 
-
-def contador_digitos():
-    if numero == 0:
-        return
-    else:
-        contar_numero = count.numero
-        print(f"el total de digitos es: {contar_numero}")
-
-numero = int(input("Ingrese un numero: "))
-contador_digitos()
-
 def repetir_palabra(n, palabra):
     if n == 0:
        return
@@ -23,6 +12,22 @@ n = int(input("¿Cuántas veces desea repetir"))
 
 repetir_palabra(n)
 
+def contar_letra(letra):
+    if n ==  1:
+    else:
+        print (f"La letra {letra}  aparece  {n} veces")
 
-de
+palabra_contar = input("Ingrese la cadena: ")
+ingrese_letra = input("Ingrese la letra que desee contar de la cadena: ")
+print(contar_letra(letra))
+
+def contador_digitos():
+    if numero == 0:
+        return
+    else:
+        contar_numero = count.numero
+        print(f"el total de digitos es: {contar_numero}")
+
+numero = int(input("Ingrese un numero: "))
+contador_digitos()
 
