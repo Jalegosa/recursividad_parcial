6a8f4dda2109f2ba32571d02772708051bed55ce
Jalegosa <jalegosa01@gmail.com>
2025-07-30 18:46:01 -0600

    cont

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 from itertools import count
 
 
-def contador_digitos(self, numero):
-    self.numero = numero
+def contador_digitos():
     if numero == 0:
         return
     else:
-        contar_numero = count(numero)
+        contar_numero = count.numero
         print(f"el total de digitos es: {contar_numero}")
 
 numero = int(input("Ingrese un numero: "))
-contador_digitos(numero)
+contador_digitos()
+
+def repetir_palabra(n, palabra):
+    if n == 0:
+       return
+    else:
+        palabra =
+        print(palabra)
+
+palabra = input("Ingrese una palabra: ")
+n = int(input("¿Cuántas veces desea repetir"))
+
+repetir_palabra(n)
+
+
+de
 
